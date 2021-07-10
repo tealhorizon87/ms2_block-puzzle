e545a77173853484d71d7b307565fd6d3769c23d
tealhoion87 <nealtoms@gmail.com>
2021-07-10 11:22:08 +0100

    Add modal for rules and controls
      - add a modal containing the rules and controls for the game and style
        appropriately
      - add JS statements to the game file in order to make the modals work
      - combine the FOR loops for the grid construction into a single
        function

--- assets/game.js
+++ assets/game.js
@@ -1,10 +1,35 @@
+// variables for game-grid and preview-grid
 var gameGrid = document.getElementById('mainGrid');
 var previewGrid = document.getElementById('previewGrid');
 var gridSquare = document.createElement('div');
 
-for (let i = 0; i < 200; i++) {
-  gameGrid.appendChild(gridSquare.cloneNode(true));
+// variables for modal operations
+var modal = document.getElementById('instructionsModal');
+var instructionsButton = document.getElementById('instructionsButton');
+var close = document.getElementsByClassName('close')[0];
+
+// FOR loops to populate the game-grid and preview-grid
+function drawGrids() {
+  for (let i = 0; i < 200; i++) {
+    gameGrid.appendChild(gridSquare.cloneNode(true));
+  }
+  for (let i = 0; i < 9; i++) {
+    previewGrid.appendChild(gridSquare.cloneNode(true));
+  }
 }
-for (let i = 0; i < 9; i++) {
-  previewGrid.appendChild(gridSquare.cloneNode(true));
+
+// modal operations
+instructionsButton.onclick = function() {
+  modal.style.display = 'block';
 }
+close.onclick = function() {
+  modal.style.display = 'none';
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+}
+
+// call necessary functions
+drawGrids()
